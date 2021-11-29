b0f5990f21c1bec271ebc874f5a7c01ab5669ca7
mastergod28 <93343280+mastergod28@users.noreply.github.com>
2021-11-29 16:44:45 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,91 @@
+import React, {useState,useEffect} from 'react';
+import { Text, View, Button, Alert } from 'react-native';
+import {css} from './assets/css/Css';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import {Login, Home, Rastreio} from './view';
+import AreaRestrita from './view/arearestrita/AreaRestrita';
+import Constants from 'expo-constants';
+import * as Permissions from 'expo-permissions';
+import {Notifications} from 'expo';
+import config from './config/config';
+
+export default function App() {
+
+  const Stack = createStackNavigator();
+  const [expoPushToken, setExpoPushToken] = useState(null);
+
+  useEffect(()=>{
+    registerForPushNotificationsAsync();
+  },[]);
+  
+  useEffect(()=>{
+    if(expoPushToken != null){
+        sendToken();
+    }
+  },[expoPushToken]);
+
+  //Registra o token do usuário
+async function registerForPushNotificationsAsync(){
+  if (Constants.isDevice) {
+      const { status: existingStatus } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+      let finalStatus = existingStatus;
+      if (existingStatus !== 'granted') {
+          const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+          finalStatus = status;
+      }
+      if (finalStatus !== 'granted') {
+          alert('Failed to get push token for push notification!');
+          return;
+      }
+      const token = await Notifications.getExpoPushTokenAsync();
+      setExpoPushToken(token);
+  } else {
+      alert('Must use physical device for Push Notifications');
+  }
+
+  if (Platform.OS === 'android') {
+      Notifications.createChannelAndroidAsync('default', {
+          name: 'default',
+          sound: true,
+          priority: 'max',
+          vibrate: [0, 250, 250, 250],
+      });
+  }
+}
+
+//Envio do token
+async function sendToken()
+{
+  let response=await fetch(config.urlRoot+'token',{
+      method:'POST',
+      headers:{
+          Accept:'application/json',
+          'Content-Type':'application/json'
+      },
+      body: JSON.stringify({
+          token: expoPushToken
+      })
+  });
+}
+
+  return (
+      <NavigationContainer>
+        <Stack.Navigator>
+        <Stack.Screen
+                  name="Home"
+                  component={Home}
+                  options={{
+                  title:"Rastreador",
+                  headerStyle:{backgroundColor:"#000000"},
+                  headerTintColor:'#F8F8FF',
+                  headerTitleStyle:{fontWeight:'bold', alignSelf:'center'}
+              }}
+          />
+          <Stack.Screen name="Login" options={{headerShown:false}} component={Login} />
+          <Stack.Screen name="Rastreio" component={Rastreio} />
+          <Stack.Screen name="AreaRestrita" options={{headerShown:false}} component={AreaRestrita} />
+        </Stack.Navigator>
+      </NavigationContainer>
+  );
+}
---
+++ Controller.js
@@ -0,0 +1,194 @@
+const express=require('express');
+const cors=require('cors');
+const bodyParser=require('body-parser');
+const models=require('./models');
+const QRCode = require('qrcode');
+const {Expo} = require('expo-server-sdk');
+
+
+const app=express();
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('assets'));
+let user=models.User;
+let tracking=models.Tracking;
+let product=models.Product;
+
+app.post('/login',async (req,res)=>{
+    let response=await user.findOne({
+        where:{name:req.body.name, password: req.body.password}
+    });
+    if(response === null){
+        res.send(JSON.stringify('error'));
+    }else{
+        res.send(response);
+    }
+});
+
+let port=process.env.PORT || 3000;
+app.listen(port,(req,res)=>{
+    console.log('Servidor Rodando');
+});
+app.post('/verifyPass',async (req,res)=>{
+    let response=await user.findOne({
+        where:{id:req.body.id, password: req.body.senhaAntiga}
+    });
+    if(response === null){
+        res.send(JSON.stringify('Senha antiga não confere'));
+    }else{
+        if(req.body. novaSenha === req.body.confNovaSenha){
+            response.password=req.body.novaSenha;
+            response.save();
+            res.send(JSON.stringify('Senha atualizada com sucesso!'));
+        }else{
+            res.send(JSON.stringify('Nova Senha e Confirmação não conferem!'));
+        }
+    }
+});
+//Criação do produto no banco
+app.post('/create',async (req,res)=>{
+    let trackingId='';
+   await tracking.create({
+     userId: req.body.userId,
+       code: req.body.code,
+       local: req.body.local
+   }).then((response)=>{
+       trackingId+=response.id;
+   });
+
+   await product.create({
+       trackingId: trackingId,
+       name: req.body.product
+   });
+
+   QRCode.toDataURL(req.body.code).then(url=>{
+    QRCode.toFile(
+        './assets/img/code.png',
+        req.body.code
+    );
+    res.send(JSON.stringify(url));
+})
+});
+//Pegar os dados do produto
+app.post('/searchProduct', async (req,res)=>{
+    let response=await tracking.findOne({
+        include:[{model:product}],
+        where: {code: req.body.code}
+    });
+    res.send(JSON.stringify(response));
+});
+//Update dos dados da mercadoria
+app.post('/update', async (req,res)=>{
+    let response=await tracking.findOne({
+        where: {code: req.body.code},
+        include: [{all:true}]
+    });
+    response.local=req.body.local;
+    response.updatedAt=new Date();
+    response.Products[0].name=req.body.product;
+    response.save();
+    response.Products[0].save();
+    res.send(JSON.stringify('Dados foram atualizados com sucesso!'));
+ });
+ //Exibir o local do rastreio
+app.post('/rastreio', async (req,res)=>{
+    let response=await tracking.findOne({
+        where:{code:req.body.code},
+        include:[{all:true}]
+    });
+    if(response === null){
+        res.send(JSON.stringify(`Nenhum produto encontrado`));
+    }else{
+        res.send(JSON.stringify(`Sua encomenda ${response.Products[0].name} já está a caminho ${response.local}.`))
+    }
+});
+
+let token=models.Token;
+//Grava o token no banco
+app.post('/token',async(req,res)=>{
+    let response=await token.findOne({
+        where:{token:req.body.token}
+    });
+    if(response == null){
+        token.create({
+            token: req.body.token,
+            createdAt: new Date(),
+            updatedAt: new Date()
+        });
+    }
+});
+let expo = new Expo();
+//Envio das notificações
+app.get('/notifications',async (req,res)=>{
+    let messages = [];
+    let somePushTokens=[];
+
+    let response=await token.findAll({
+        raw: true
+    });
+    response.map((elem,ind,obj)=>{
+       somePushTokens.push(elem.token);
+    });
+
+    for (let pushToken of somePushTokens) {
+
+        if (!Expo.isExpoPushToken(pushToken)) {
+            console.error(`Push token ${pushToken} is not a valid Expo push token`);
+            continue;
+        }
+
+        messages.push({
+            to: pushToken,
+            sound: 'default',
+            title: 'WefLog',
+            body: 'Bem vindo ao nosso app!',
+            data: { withSome: 'data' },
+        })
+    }
+    let chunks = expo.chunkPushNotifications(messages);
+    let tickets = [];
+    (async () => {
+        for (let chunk of chunks) {
+            try {
+                let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+                console.log(ticketChunk);
+                tickets.push(...ticketChunk);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })();
+    let receiptIds = [];
+    for (let ticket of tickets) {
+        if (ticket.id) {
+            receiptIds.push(ticket.id);
+        }
+    }
+
+    let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+    (async () => {
+        for (let chunk of receiptIdChunks) {
+            try {
+                let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+                console.log(receipts);
+
+                for (let receiptId in receipts) {
+                    let { status, message, details } = receipts[receiptId];
+                    if (status === 'ok') {
+                        continue;
+                    } else if (status === 'error') {
+                        console.error(
+                            `There was an error sending a notification: ${message}`
+                        );
+                        if (details && details.error) {
+                            console.error(`The error code is ${details.error}`);
+                        }
+                    }
+                }
+            } catch (error) {
+                console.error(error);
+            }
+        }
+    })();
+});
